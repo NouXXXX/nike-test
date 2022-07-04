b1499d7fe08f6d9bf0d5c44995b595bbc9aeacbe
NouXXXX <leedh50778037@gmail.com>
2022-07-04 20:33:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Featured from './components/featured/Featured'
+import Video from './components/video'
+import Shoes from './components/shoes/Shoes'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Video />
+      <Shoes />
+      <Featured />
+      <Footer />
+    </>
   );
 }
 
