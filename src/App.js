d3ac8e6285b169cf42d29d3d181c48cf3fe14adb
ksj051203 <ksj054703@gmail.com>
2022-07-05 20:35:35 +0900

    image grid

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import Video from './components/video'
 import Shoes from './components/shoes/Shoes'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
-
+import Purchase from './components/purchase/Purchase'
 function App() {
   return (
     <>
-      <Navbar />
-      <Video />
-      <Shoes />
-      <Featured />
-      <Footer />
+    <Navbar />
+      <Purchase />
     </>
   );
 }
